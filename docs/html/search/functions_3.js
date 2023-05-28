6c903dd4f4b5eb0298d66357a579353e7701b540
Corey Pennycuff <pennycuff.c@gmail.com>
2023-05-27 21:02:00 -0500

    update documentation

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['operator_2b_29',['operator+',['../classGhoti_1_1shared__string__view.html#a9d5c71bbaea2c9e696dc42d86b23c70a',1,'Ghoti::shared_string_view']]],
-  ['operator_2b_3d_30',['operator+=',['../classGhoti_1_1shared__string__view.html#abd7183a38587121b942292c663b6a41e',1,'Ghoti::shared_string_view']]],
+  ['operator_2b_3d_30',['operator+=',['../classGhoti_1_1shared__string__view.html#a4afcc2632d6c483f77ff343431678d7f',1,'Ghoti::shared_string_view']]],
   ['operator_3c_3c_31',['operator&lt;&lt;',['../shared__string__view_8hpp.html#a1ca6b2d32e9ac7688ec958375061ac15',1,'Ghoti']]],
-  ['operator_3c_3d_3e_32',['operator&lt;=&gt;',['../classGhoti_1_1shared__string__view.html#a1e886acb9f33a2c7fb8f91789ad2c9e2',1,'Ghoti::shared_string_view']]],
-  ['operator_3d_3d_33',['operator==',['../classGhoti_1_1shared__string__view.html#a9d8138f50059c1aa19de8f9039a975dd',1,'Ghoti::shared_string_view']]],
+  ['operator_3c_3d_3e_32',['operator&lt;=&gt;',['../classGhoti_1_1shared__string__view.html#a7a01c48f3c9bf8225accd3ed3670854a',1,'Ghoti::shared_string_view']]],
+  ['operator_3d_3d_33',['operator==',['../classGhoti_1_1shared__string__view.html#a84019c7bd20bffb2087d0b7e825b5e2b',1,'Ghoti::shared_string_view']]],
   ['operator_5b_5d_34',['operator[]',['../classGhoti_1_1shared__string__view.html#a987bad210f27930c7788d1adf4b395b3',1,'Ghoti::shared_string_view']]],
   ['string_5fview_35',['string_view',['../classGhoti_1_1shared__string__view.html#a40193d831416d406c0a8ec8ef2a5ae0b',1,'Ghoti::shared_string_view']]]
 ];
