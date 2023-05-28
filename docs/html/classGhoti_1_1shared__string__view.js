e65d210572261c0c7e690cec03861912077d47d4
Corey Pennycuff <pennycuff.c@gmail.com>
2023-05-27 21:14:50 -0500

    update documentation

--- docs/html/classGhoti_1_1shared__string__view.js
+++ docs/html/classGhoti_1_1shared__string__view.js
@@ -1,9 +1,10 @@
 var classGhoti_1_1shared__string__view =
 [
+    [ "shared_string_view", "classGhoti_1_1shared__string__view.html#a6d4523491fd622200e416600f8e30e60", null ],
     [ "shared_string_view", "classGhoti_1_1shared__string__view.html#ab56e9e86ffdf8697aef5561411938b32", null ],
     [ "shared_string_view", "classGhoti_1_1shared__string__view.html#ad25885249718a47f82ef96e257d4a076", null ],
     [ "shared_string_view", "classGhoti_1_1shared__string__view.html#a55930d10c52128248d31ce9f127a3b06", null ],
-    [ "shared_string_view", "classGhoti_1_1shared__string__view.html#a6d4523491fd622200e416600f8e30e60", null ],
+    [ "shared_string_view", "classGhoti_1_1shared__string__view.html#a32ecb472f5c92b0b7ac050b1cb875136", null ],
     [ "begin", "classGhoti_1_1shared__string__view.html#aba455da5a4d2db1274370174aa9e022e", null ],
     [ "end", "classGhoti_1_1shared__string__view.html#a8020ac49303481ad7aae3f007b2d84fc", null ],
     [ "length", "classGhoti_1_1shared__string__view.html#a4bcdec6f77c3b85210bbe5f2b965558f", null ],
