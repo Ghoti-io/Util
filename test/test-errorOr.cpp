9346aa305514050a5ff32445ba315d56c1fbfcac
Corey Pennycuff <pennycuff.c@gmail.com>
2023-07-08 00:33:33 -0500

    use custom error code in test-ErrorOr

--- test/test-errorOr.cpp
+++ test/test-errorOr.cpp
@@ -13,6 +13,45 @@
 using namespace std;
 using namespace Ghoti;
 
+namespace Ghoti::Test {
+enum class ErrorCode {
+  NOERROR = 0,
+  ERROR1,
+  ERROR2,
+};
+
+class ErrorCategory : public std::error_category {
+  public:
+  virtual const char* name() const noexcept {
+    return "Ghoti::Test";
+  }
+  virtual string message(int ev) const {
+    static unordered_map<ErrorCode, string> description {
+      {ErrorCode::NOERROR, "No Error"},
+      {ErrorCode::ERROR1, "Error 1"},
+      {ErrorCode::ERROR2, "Error 2"},
+    };
+    auto ec = static_cast<ErrorCode>(ev);
+    return description.contains(ec)
+      ? description[ec]
+      : "";
+  }
+};
+
+static const ErrorCategory constErrorCategory{};
+
+std::error_code make_error_code(Ghoti::Test::ErrorCode ec) {
+  return {static_cast<int>(ec), constErrorCategory};
+}
+}
+
+namespace std {
+template <>
+struct is_error_code_enum<Ghoti::Test::ErrorCode> : true_type {};
+}
+
+using namespace Ghoti::Test;
+
 TEST(ErrorOr, Constructor) {
   {
     // Default Contructor should not be an error, and should have the default
@@ -31,11 +70,11 @@ TEST(ErrorOr, Constructor) {
   }
   {
     // Contructor with a std::error_code should be fine, too
-    error_code ec = make_error_code(errc::broken_pipe);
+    std::error_code ec{ErrorCode::ERROR1};
     Util::ErrorOr<int> e{ec};
     ASSERT_FALSE(e);
     ASSERT_TRUE(e.error_code());
-    ASSERT_EQ(e.error_code(), errc::broken_pipe);
+    ASSERT_EQ(e.error_code(), ErrorCode::ERROR1);
     ASSERT_ANY_THROW(*e);
   }
 }
